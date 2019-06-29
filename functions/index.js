0d554eede13f7f1a7198dc07734e639181627c8a
Dun-Njuguna <duncankariuki60@gmail.com>
2019-06-29 03:32:31 +0300

    initial commit

---
+++ functions/index.js
@@ -0,0 +1,19 @@
+const functions = require('firebase-functions');
+
+const express = require('express');
+const app = express(); 
+
+const FBAuth = require('./util/fbAuth');
+
+const {getAllScreams, postOneScream} = require('./handlers/screams');
+const{signUp, login} = require('./handlers/users');
+
+// scream routes
+app.get('/screams', getAllScreams);
+app.post('/scream', FBAuth, postOneScream);
+
+// users routes
+app.post('/signup', signUp)
+app.post(`/login`, login)
+
+exports.api = functions.https.onRequest(app); 
---
+++ functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports= {
+  apiKey: "AIzaSyDIkVg9yIYTGT_75sOCzmW4Z1RxOj4xQJ8",
+  authDomain: "react1-4ad5b.firebaseapp.com",
+  databaseURL: "https://react1-4ad5b.firebaseio.com",
+  projectId: "react1-4ad5b",
+  storageBucket: "react1-4ad5b.appspot.com",
+  messagingSenderId: "374018603976",
+  appId: "1:374018603976:web:aee9107481d9f4da"
+  };
